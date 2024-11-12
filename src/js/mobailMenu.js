bd1f2094dbfcbfd9a31d7c22f7ddfed9ead4f73a
Yulia-As-jpg <yulkahitrova26@gmail.com>
2024-11-12 22:16:51 +0300

    making changes

--- src/js/mobailMenu.js
+++ src/js/mobailMenu.js
@@ -2,11 +2,13 @@ import {burgerBtn, mobailMenu, visibleMenu} from './variables';
 
 function toggleMobileMenu() {
   if (mobailMenu.style.display === 'block') {
+    mobailMenu.style.position = 'fixed';
     mobailMenu.style.display = 'none';
     visibleMenu.style.display = 'none';
     visibleMenu.style.opacity = '0';
   } else {
     mobailMenu.style.display = 'block';
+    mobailMenu.style.position = 'fixed';
     visibleMenu.style.display = 'block';
     visibleMenu.style.opacity = '0.9';
   }
